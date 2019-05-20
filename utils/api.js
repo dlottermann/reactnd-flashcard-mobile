14e4a8d637ba521ff8abdf0b6caddff62bef7594
dlottermann <diegolottermann@gmail.com>
2019-05-20 15:40:23 -0300

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -9,10 +9,24 @@ export const getDecks = () => {
     return AsyncStorage.getItem(FLASHCARD_STORAGE_KEY).then(formatDecksResults)
 }
 
-
 // getDeck: dado um único argumento id, ele retorna o baralho associado àquele id. 
+export const getDeck = (id) => {
+
+}
 
 // saveDeckTitle: dado um único argumento title, ele adiciona-o aos baralhos. 
 
+export const saveDeckTitle = ({deckTitle, key}) => {
+
+}
+
 
 // addCardToDeck: dado dois argumentos, title e card, ele adiciona o cartão à lista de perguntas ao baralho com o título associado. 
+export const addCardToDeck = (card) => {
+
+}
+
+//RemoveDeck: dado um único argumento title (ou ID), ele remove um baralho. 
+export const removeDeck = (id) => {
+
+}
